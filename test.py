426ecf948014635850c26c2b878f9452677e2313
JedizR <teenday.jedi@gmail.com>
2024-09-12 16:28:43 +0700

    fibo try

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
-import math
+def fibo(n,memo : list):
+    if n in memo:
+        return memo[n]
+    if n == 1 or n == 2 :
+        result = 1
+    else:
+        result = fibo(n-1, memo) + fibo(n-2, memo)
+    memo[n] = result
+    return result
+mem = {}
+fibo(1000,mem)
+print(mem)
 
-print(f'{math.pi} nice')
